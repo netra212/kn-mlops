199ae69bd36d41c295100d9f004e40dea2f8186a
netra212 <netra200021kcbdr@gmail.com>
2025-06-20 11:39:02 +0100

    first commit

---
+++ datascienceproject/main.py
@@ -0,0 +1,53 @@
+from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+
+# ------------------------------------------------------------------------------------------------ #
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<< \n\nx=======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+# ------------------------------------------------------------------------------------------------ #
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<< \n\n x========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+# ------------------------------------------------------------------------------------------------ #
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.initiate_data_transformation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<< \n\n x========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+# ------------------------------------------------------------------------------------------------ #
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = ModelTrainerTrainingPipeline()
+    obj.initiate_model_training()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<< \n\n x========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ datascienceproject/src/datascience/pipeline/data_transformation_pipeline.py
@@ -0,0 +1,26 @@
+from src.datascience.config.configuration import ConfigurationManager
+from src.datascience.components.data_transformation import DataTransformationConfig
+from src.datascience import logger
+from pathlib import Path
+
+STAGE_NAME = "Data Transformation stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_data_transformation(self):
+        try:
+            with open("artifacts/data_validation/status.txt") as f:
+                status = f.read().split(' ')[-1]
+            
+            if status == "True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformationConfig(config=data_transformation_config)
+                data_transformation.train_test_splitting()
+            else:
+                raise Exception("Your data scheme is not valid.")
+            
+        except Exception as e:
+            print(e)
